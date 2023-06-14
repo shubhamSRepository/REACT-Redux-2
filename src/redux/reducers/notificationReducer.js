6960a0aa8ddb6d3324615969c033cd7b5ec7eb91
Shubham Soni <soni.shubham927@gmail.comgit config --global user.email soni.shubham927@gmail.com>
2023-06-14 14:19:02 +0530

    extra reducer using map

--- src/redux/reducers/notificationReducer.js
+++ src/redux/reducers/notificationReducer.js
@@ -14,7 +14,7 @@ const notificationSlice = createSlice({
         }
     },
 
-    /**Extra Reducer method 1 */
+    /**Extra Reducer method 1 (this method is not recommended to use) */
     // extraReducers: {
     //     /**'extraReducer' helps us to do actions which are based on another reducer or slice
     //      * for eg- in this extra reducer an alert will appear when "to-do list " adds any new task.
@@ -23,16 +23,26 @@ const notificationSlice = createSlice({
     //         state.message = "Todo is created";
     //     }
     // }
-    
 
-    /**Extra Reducer method 2 
-     * 'builder' is an inbuilt api
+
+    /**Extra Reducer method 2 using 'builder'.
+     * 'builder' is an inbuilt api.
     */
-    extraReducers: (builder) => {
-        builder.addCase(action.add, (state, action) => {
-            state.message = "Todo is created";
-        })
+    // extraReducers: (builder) => {
+    //     builder.addCase(action.add, (state, action) => {
+    //         state.message = "Todo is created";
+    //     })
+    // }
+
+
+    /**Extra Reducer method 3 using 'map'.*/
+    extraReducers: {
+        //map objects:[key]:value
+        [action.add]: (state, action) => {
+            state.message = "To Do is created";
+        }
     }
+
 })
 
 export const notificationReducer = notificationSlice.reducer;
